6b2d6b411ccd889384d8437094b2dec89abebb31
Timofeyka) <tbahilin@gmail.com>
2025-06-25 01:02:25 +0700

    Update User.cs
    убрал табы лишнии

--- BabuinMarketBackend/BabuinMarket.API/BabuinMarket.Domain/User.cs
+++ BabuinMarketBackend/BabuinMarket.API/BabuinMarket.Domain/User.cs
@@ -2,8 +2,8 @@
 
 public class User
 {
-	public int Id{ get; set; }
+    public int Id { get; set; }
     public string? UserName { get; set; }
     public List<Role>? Roles{ get; set; }
-	public string? Email { get; set; }
+    public string? Email { get; set; }
 }
